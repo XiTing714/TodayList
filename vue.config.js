bfda49965a3a72fb23f271b2a26d42f542fd8807
ChenXiaoting714 <825415750@qq.com>
2021-01-15 19:44:40 +0800

    vue config

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'components': '@/components',
+        'content': 'components/content',
+        'common': 'components/common'
+      }
+    }
+  }
+}

76553534351a1ae36d354a7f3a3d5ae618a6f1c2
ChenXiaoting714 <825415750@qq.com>
2021-02-19 22:11:53 +0800

    change logo icon

--- src/views/SkinChange/SkinChange.vue
+++ src/views/SkinChange/SkinChange.vue
@@ -6,6 +6,10 @@
       :skinColor="item">
       </skin-item>
     </skin>
+    <!-- <div class="img">
+      <img src="~assets/img/flower1.png" id="flo1">
+      <img src="~/assets/img/flower2.png" id="flo2">
+    </div> -->
   </div>
 </template>
 
@@ -38,4 +42,27 @@ export default {
 .skin {
   margin: 100px auto;
 }
+
+/* .img {
+  margin-top: -150px;
+  border: solid 1px;
+  position: relative;
+}
+
+#flo1 {
+  width: 300px;
+  height: 300px;
+  position: absolute;
+  top: -150px;
+
+}
+
+#flo2 {
+  width: 200px;
+  height: 200px;
+  position: absolute;
+  left: 450px;
+}
+ */
+
 </style>

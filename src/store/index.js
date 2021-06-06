67ec58f387a66f6bc7c0cacf12a0547339234503
ChenXiaoting714 <825415750@qq.com>
2021-06-06 21:33:54 +0800

    fix merge problem

--- src/store/index.js
+++ src/store/index.js
@@ -7,14 +7,9 @@ import getters from './getters'
 Vue.use(Vuex)
 
 const state = {
-<<<<<<< Updated upstream
-  appBgColor: JSON.parse(window.localStorage.getItem('appBgColor')) || 'green',
-  filterText: 'all'
-=======
   appBgColor: JSON.parse(window.localStorage.getItem('appBgColor')) || '#ff9900',
   filterText: 'all',
   allList: JSON.parse(window.localStorage.getItem('toDoList') || '[]')
->>>>>>> Stashed changes
 }
 
 const store = new Vuex.Store({
